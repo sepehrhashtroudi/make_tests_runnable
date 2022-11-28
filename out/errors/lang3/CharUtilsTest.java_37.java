2269b80c86675e538f6068217230d664fe888256
Sepehr <sepehrhashtroudi@gmail.com>
2022-11-28 15:39:55 -0700

    before ji-ho

--- out/errors/lang3/CharUtilsTest.java_37.java
+++
@@ -1,6 +0,0 @@
-@Test
-public void testToUnicodeEscaped_Character5535() { 
-     assertEquals(null, CharUtils.unicodeEscaped(null)); 
-     assertEquals("\\u0041", CharUtils.unicodeEscaped(CHARACTER_A)); 
- } 
-@SuppressWarnings("deprecation")
