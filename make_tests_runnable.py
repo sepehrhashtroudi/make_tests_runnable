ff0a16362b34858fd323c27bea1ef1a81076c226
Sepehr <sepehrhashtroudi@gmail.com>
2022-11-30 15:30:35 -0700

    remove identical tests from runnable folder

--- make_tests_runnable.py
+++ make_tests_runnable.py
@@ -4,8 +4,8 @@ import shutil
 root = './model_predictions/'
 
 # gold = "test_0.gold"
-# out = "test_best-bleu.output"
-# info = "test_info.txt"
+out = "test_best-bleu.output"
+info = "test_info.txt"
 #out = "train.tests"
 #info = "train_info.txt"
 # out = "test.tests"
@@ -13,8 +13,8 @@ root = './model_predictions/'
 # out = "concat.tests"
 # info = "concat_info.txt"
 
-out = "concat.tests"
-info = "concat_info.txt"
+# out = "concat.tests"
+# info = "concat_info.txt"
 
 
 
@@ -42,16 +42,20 @@ def prepare():
         shutil.rmtree("out/")
     print(len(out_lines))
     print(len(info_lines))
+    list_of_tests = []
     cnt = 0
     for line, info_line in zip(out_lines, info_lines):
-        line = re.sub('test\w+\(\)', replace, line)
-        line = re.sub("\[EOL\]", '\n', line)
-        path = info_line.split("<test_path>:")[-1].strip()
-        os.makedirs(os.path.dirname("out/runnable_tests/" + path), exist_ok=True)
-        with open("out/runnable_tests/" + path, "a") as Final_tests:
-            # print(cnt, line)
-            Final_tests.write(line)
-        cnt += 1
+        if line not in list_of_tests:
+            list_of_tests.append(line)
+            line = re.sub('test\w+\(\)', replace, line)
+            line = re.sub("\[EOL\]", '\n', line)
+            path = info_line.split("<test_path>:")[-1].strip()
+            os.makedirs(os.path.dirname("out/runnable_tests/" + path), exist_ok=True)
+            with open("out/runnable_tests/" + path, "a") as Final_tests:
+                # print(cnt, line)
+                Final_tests.write(line)
+            cnt += 1
+
         
 
 if __name__ == "__main__":
